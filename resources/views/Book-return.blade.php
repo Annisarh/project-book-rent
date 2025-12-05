f333658e568dc0aa3ce467c1bf4ae919b37cdfdb
annisa rahmadani <annisarahmadani2311@gmail.com>
2025-12-05 08:08:21 +0700

    finish

--- resources/views/Book-return.blade.php
+++ resources/views/Book-return.blade.php
@@ -47,7 +47,6 @@
     </div>
     {{-- bagian table ends --}}
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
     // In your Javascript (external .js resource or <script> tag)
--- resources/views/Books.blade.php
+++ resources/views/Books.blade.php
@@ -11,8 +11,8 @@
             {{ session('success') }}
         </div>
     @endif
-    <div class="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default mb-4">
-        <table class="w-full text-sm text-left rtl:text-right text-body">
+    <div class="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default mb-4 p-5">
+        <table id="myTable" class="w-full text-sm text-left rtl:text-right text-body">
             <thead class="text-sm text-body bg-neutral-secondary-soft border-b rounded-base border-default">
                 <tr>
                     <th scope="col" class="px-6 py-3 font-medium">
